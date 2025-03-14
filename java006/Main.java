738056c77605d112f76cb99a35b9b0a06d5710aa
Geronimo Viscontti <visconttig@gmail.com>
2025-03-14 10:13:35 -0300

    Complete exercise.

--- java006/Main.java
+++ java006/Main.java
@@ -17,7 +17,16 @@ public class Main {
         C = sc.nextDouble();
         sc.close();
         final double PI = 3.14159;
-        final double TRIANGULO = (A * C) / 2;
-        final double CIRCULO = (PI * (Math.pow(C, 2)));
+        final double TRIANGLE = ((A * C) / 2);
+        final double CIRCLE = (PI * (Math.pow(C, 2)));
+        final double TRAPEZOID = (((A + B) / 2) * C);
+        final double SQUARE = (Math.pow(B, 2));
+        final double RECTANGLE = (A * B);
+
+        System.out.printf("TRIANGLE: %.2f%n", TRIANGLE);
+        System.out.printf("CIRCLE: %.2f%n", CIRCLE);
+        System.out.printf("TRAPEZOID: %.2f%n", TRAPEZOID);
+        System.out.printf("SQUARE: %.2f%n", SQUARE);
+        System.out.printf("RECTANGLE: %.2f%n", RECTANGLE);
     }
 }
