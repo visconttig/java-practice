3b3fab263686e09384cb68761db7a50e71294ee9
Geronimo Viscontti <visconttig@gmail.com>
2025-04-03 11:23:05 -0300

    ...

--- java015/src/Main.java
+++ java015/src/Main.java
@@ -3,10 +3,10 @@ import java.util.Locale;
 
 public class Main {
     public static void main(String[] args){
+        Locale.setDefault(Locale.US);
        Scanner sc = new Scanner(System.in);
        double salary = 0.00;
-       double taxes = 50;
-       double salaryAfterTaxes = salary * (1 + (taxes / 100));
+       double taxes = 0.00;
        System.out.print("Enter the salary: ");
        salary = sc.nextDouble();
 
@@ -16,26 +16,30 @@ public class Main {
            if(salary <= 2000){
                // exempt from taxes
                taxes = 0.00;
-               System.out.printf("Exempt from taxes: %.2f%n", salaryAfterTaxes);
+               System.out.printf("Exempt from taxes: %.2f%n", applyTaxes(salary, taxes));
            } else {
                if(salary <= 3000){
                    // apply 8%
                    taxes = 8.00;
-                   System.out.printf("Applied 8 per cent of taxes: %.2f%n", salaryAfterTaxes);
+                   System.out.printf("Applied 8 per cent of taxes: %.2f%n", applyTaxes(salary, taxes));
                } else {
                    if(salary <= 4500){
                        // apply 18%
                        taxes = 18.00;
-                       System.out.printf("Applied 18 per cent of taxes: %.2f%n", salaryAfterTaxes);
+                       System.out.printf("Applied 18 per cent of taxes: %.2f%n", applyTaxes(salary, taxes));
                    } else {
                        // apply 28%
                        taxes = 28.00;
-                       System.out.printf("Applied 28 per cent of taxes: %.2f%n", salaryAfterTaxes);
+                       System.out.printf("Applied 28 per cent of taxes: %.2f%n", applyTaxes(salary, taxes));
                    }
                }
            }
        }
 
        sc.close();
+    }
+
+    private static double applyTaxes(double salary, double taxes){
+        return salary * (1 + (taxes / 100));
     }
 }
